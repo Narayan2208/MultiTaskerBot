83eb87f79dab34017ecba59b8809f554a652ab8c
Narayan2208 <narayandutta2208@gmail.com>
2023-05-29 12:48:46 +0530

    changes few things

--- src/index.js
+++ src/index.js
@@ -1,30 +1,31 @@
-
-
 const TelegramBot = require("node-telegram-bot-api");
-const googleTranslate = require('google-translate');
+const googleTranslate = require("google-translate");
 const axios = require("axios");
 const quotes = require("./quotes");
-const schedule = require('node-schedule');
-const notifier = require('node-notifier');
+const schedule = require("node-schedule");
+const notifier = require("node-notifier");
 require("dotenv").config();
 
-const request = require('request');
-const { google } = require('googleapis');
-const OpenAI = require('openai');
+const request = require("request");
+const { google } = require("googleapis");
+const OpenAI = require("openai");
 
 // Telegram Bot token
 const telegramToken = process.env.TELEGRAM_TOKEN;
-
 
 // Create a new Telegram Bot instance
 const bot = new TelegramBot(telegramToken, { polling: true });
 
 bot.onText(/\/start/, (message) => {
-  const wikiRandomApiUrl2 = "https://en.wikipedia.org/api/rest_v1/page/random/summary";
+  const wikiRandomApiUrl2 =
+    "https://en.wikipedia.org/api/rest_v1/page/random/summary";
   bot.sendMessage(
     message.chat.id,
-    `Welcome ${message.from.first_name} 😊 to MultiTaskerBot 🤖! We're thrilled to have you here. Our bot specializes in fetching Wikipedia articles to provide you with instant knowledge on various topics. Just send a search query or a specific topic, and we'll fetch the relevant article for you. Feel free to explore and expand your knowledge with us. If you have any questions or need assistance, don't hesitate to ask. Enjoy exploring the world of information!  \n\n Here some command to use this Bot 🤖! \n /start -> to start the Bot \n /stop -> for stop the Bot  \n\n Type anything but in one word ! it will give you the results. \n Like : /search india,  \n /git enter your git username here,  \n /weather enter city name`
+    `Welcome ${message.from.first_name} 😊 to our MultiTaskerBot 🤖!
+
+    Get random articles, search results, quotes, quiz answers, weather updates, reminders, and worldwide news. Enjoy a seamless experience with our bot. Happy multitasking! 🌟  \n\n Here some command to START and STOP this Bot 🤖! \n /start -> to start the Bot \n /stop -> for stop the Bot`
   );
+  bot.sendMessage(message.chat.id, ` \n\n Here is list of commands you can explore \n /search india,  \n /git enter your git username here,  \n /weather enter city name \n /quote \n /quiz \n /newstopic \n /newscategory \n /reminder take a break at 3:00`);
   bot.sendMessage(message.chat.id, `Here is some random result for you 🔴`);
   postRandomWikiArticle2(wikiRandomApiUrl2, message.chat.id);
 });
@@ -33,7 +34,7 @@ bot.onText(/\/stop|\/deactivate/, (message) => {
   bot.sendMessage(
     message.chat.id,
     `Thank you for using MultiTaskerBot 🤖! \n\n 
-    We hope you found our Wikipedia articles informative and useful. If you have any more queries in the future, feel free to reach out. To stop receiving articles or deactivate the bot, simply type '/stop' or '/deactivate'. We appreciate your support and hope to see you again soon!`
+    We hope you found our bot informative and useful. If you have any more queries in the future, feel free to reach out. To stop receiving articles or deactivate the bot, simply type '/stop' or '/deactivate'. We appreciate your support and hope to see you again soon!`
   );
 });
 
@@ -47,7 +48,10 @@ bot.onText(/\/git (.+)/, (message, match) => {
 bot.onText(/\/search (.+)/, (message, match) => {
   const searchKeyword = match[1];
   const wikiRandomApiUrl = `https://en.wikipedia.org/api/rest_v1/page/summary/${searchKeyword}`;
-  bot.sendMessage(message.chat.id, `Here is your full details of your git profile`);
+  bot.sendMessage(
+    message.chat.id,
+    `Here is your full details of your git profile`
+  );
   postRandomWikiArticle(wikiRandomApiUrl, message.chat.id);
 });
 
@@ -65,44 +69,62 @@ bot.onText(/\/quote/, (message) => {
   const randomIndex = Math.floor(Math.random() * quotes.length);
   const randomQuote = quotes[randomIndex];
   // console.log(message);
-  bot.sendMessage( message.chat.id, `Here is your random quote ${message.from.first_name} 😊` );
+  bot.sendMessage(
+    message.chat.id,
+    `Here is your random quote ${message.from.first_name} 😊`
+  );
   bot.sendMessage(message.chat.id, randomQuote);
 });
 
 bot.on("message", (message) => {
- if(message.text == '/newstopic'){
-   console.log(message, "oiwefhoiweu");
-    bot.sendMessage(message.chat.id, `Hey, ${message.from.first_name} 😊 please provide also country code and what news you want`);
-    bot.sendMessage(message.chat.id, `Write in this format, -> /newstopic in modi\n it will give you all modi related news,\n Try Once`);
-    bot.sendMessage(message.chat.id, `you can only search by 54 country's code,\n ae ar at au be bg br ca ch cn co cu cz de eg fr gb gr hk hu id ie il in it jp kr lt lv ma mx my ng nl no nz ph pl pt ro rs ru sa se sg si sk th tr tw ua us ve za`);
-
- }else if(message.text == '/newscategory'){
-  bot.sendMessage(message.chat.id, `Hey, ${message.from.first_name} 😊 please provide also country code and category`);
-  bot.sendMessage(message.chat.id, `Write in this format, -> /newscategory in sports \n it will give you all sports related news,\n Try Once`);
-  bot.sendMessage(message.chat.id, `here is the list of category you can get, \n business, entertainment, general, health, science, sports, technology`);
-
- }
- 
+  if (message.text == "/newstopic") {
+   
+    bot.sendMessage(
+      message.chat.id,
+      `Hey, ${message.from.first_name} 😊 please provide also country code and what news you want`
+    );
+    bot.sendMessage(
+      message.chat.id,
+      `Write in this format, -> /newstopic in modi\n it will give you all modi related news,\n Try Once`
+    );
+    bot.sendMessage(
+      message.chat.id,
+      `you can only search by 54 country's code,\n ae ar at au be bg br ca ch cn co cu cz de eg fr gb gr hk hu id ie il in it jp kr lt lv ma mx my ng nl no nz ph pl pt ro rs ru sa se sg si sk th tr tw ua us ve za`
+    );
+  } else if (message.text == "/newscategory") {
+    bot.sendMessage(
+      message.chat.id,
+      `Hey, ${message.from.first_name} 😊 please provide also country code and category`
+    );
+    bot.sendMessage(
+      message.chat.id,
+      `Write in this format, -> /newscategory in sports \n it will give you all sports related news,\n Try Once`
+    );
+    bot.sendMessage(
+      message.chat.id,
+      `here is the list of category you can get, \n business, entertainment, general, health, science, sports, technology`
+    );
+  }else if(message.text == "/reminder"){
+    bot.sendMessage(message.chat.id,`Hey, ${message.from.first_name}  please provide also reminder name and time in this format :  /reminder take a break at 3:00 \n it will send a notification.`);
+  }
 });
 
 bot.onText(/\/newstopic (.+)/, async (message, match) => {
   const countryName = match[1];
-  
 
-  const separatedWords = countryName.split(' ');
-  console.log(separatedWords); 
+  const separatedWords = countryName.split(" ");
+  console.log(separatedWords);
   console.log(match, "name");
 
   try {
-    const newsAPIUrl = 'https://newsapi.org/v2/top-headlines';
-    const newsAPIKey = '9db1ffe15f26498eb15fd32fc07a57ae'; // Replace with your actual news API key
-    
+    const newsAPIUrl = "https://newsapi.org/v2/top-headlines";
+    const newsAPIKey = "9db1ffe15f26498eb15fd32fc07a57ae"; // Replace with your actual news API key
 
     const response = await axios.get(newsAPIUrl, {
       params: {
         country: separatedWords[0], // Replace with the desired country code
         // category : separatedWords[1],
-        q : separatedWords[1],
+        q: separatedWords[1],
         apiKey: newsAPIKey,
       },
     });
@@ -112,42 +134,42 @@ bot.onText(/\/newstopic (.+)/, async (message, match) => {
     let messageChunks = [];
 
     articles.forEach((article) => {
-      const headline = `*${article.title}*\n${article.description}\n${article.author == null ? "No author" : article.author}\n${article.url}`;
+      const headline = `*${article.title}*\n${article.description}\n${
+        article.author == null ? "No author" : article.author
+      }\n${article.url}`;
       if (headline.length <= maxMessageLength) {
         messageChunks.push(headline);
       }
     });
 
     if (messageChunks.length > 0) {
-      bot.sendMessage(message.chat.id, 'Here are the latest headlines:');
+      bot.sendMessage(message.chat.id, "Here are the latest headlines:");
       messageChunks.forEach((chunk) => {
-        bot.sendMessage(message.chat.id, chunk, { parse_mode: 'Markdown' });
+        bot.sendMessage(message.chat.id, chunk, { parse_mode: "Markdown" });
       });
     } else {
-      bot.sendMessage(message.chat.id, 'No headlines available.');
+      bot.sendMessage(message.chat.id, "No headlines available.");
     }
   } catch (error) {
-    console.error('Error fetching news headlines:', error);
+    console.error("Error fetching news headlines:", error);
   }
 });
 
 bot.onText(/\/newscategory (.+)/, async (message, match) => {
   const countryName = match[1];
-  
 
-  const separatedWords = countryName.split(' ');
-  console.log(separatedWords); 
+  const separatedWords = countryName.split(" ");
+  console.log(separatedWords);
   console.log(match, "name");
 
   try {
-    const newsAPIUrl = 'https://newsapi.org/v2/top-headlines';
-    const newsAPIKey = '9db1ffe15f26498eb15fd32fc07a57ae'; // Replace with your actual news API key
-    
+    const newsAPIUrl = "https://newsapi.org/v2/top-headlines";
+    const newsAPIKey = "9db1ffe15f26498eb15fd32fc07a57ae"; // Replace with your actual news API key
 
     const response = await axios.get(newsAPIUrl, {
       params: {
         country: separatedWords[0], // Replace with the desired country code
-        category : separatedWords[1],
+        category: separatedWords[1],
         // q : separatedWords[1],
         apiKey: newsAPIKey,
       },
@@ -165,44 +187,47 @@ bot.onText(/\/newscategory (.+)/, async (message, match) => {
     });
 
     if (messageChunks.length > 0) {
-      bot.sendMessage(message.chat.id, 'Here are the latest headlines:');
+      bot.sendMessage(message.chat.id, "Here are the latest headlines:");
       messageChunks.forEach((chunk) => {
-        bot.sendMessage(message.chat.id, chunk, { parse_mode: 'Markdown' });
+        bot.sendMessage(message.chat.id, chunk, { parse_mode: "Markdown" });
       });
     } else {
-      bot.sendMessage(message.chat.id, 'No headlines available.');
+      bot.sendMessage(message.chat.id, "No headlines available.");
     }
   } catch (error) {
-    console.error('Error fetching news headlines:', error);
+    console.error("Error fetching news headlines:", error);
   }
 });
 
 const questions = [
   {
-    question: 'What is the capital of France?',
-    answer: 'Paris',
+    question: "What is the capital of France?",
+    answer: "Paris",
   },
   {
-    question: 'What is the name of the current US president?',
-    answer: 'Joe Biden',
+    question: "What is the name of the current US president?",
+    answer: "Joe Biden",
   },
   {
-    question: 'What is the chemical symbol for water?',
-    answer: 'H2O',
+    question: "What is the chemical symbol for water?",
+    answer: "H2O",
   },
 ];
 
-bot.on('message',  (message) => {
-  if (message.text === '/quiz') {
+bot.on("message", (message) => {
+  if (message.text === "/quiz") {
     const question = questions[Math.floor(Math.random() * questions.length)];
     bot.sendMessage(message.chat.id, question.question);
-    bot.on('message',  (answer) => {
+    bot.on("message", (answer) => {
       if (answer.text === question.answer) {
-        bot.sendMessage(message.chat.id, 'Correct!');
+        bot.sendMessage(message.chat.id, "Correct!");
       } else {
-        bot.sendMessage(message.chat.id, 'Incorrect! The answer was ' + question.answer);
+        bot.sendMessage(
+          message.chat.id,
+          "Incorrect! The answer was " + question.answer
+        );
       }
-      bot.removeListener('message', this);
+      bot.removeListener("message", this);
     });
   }
 });
@@ -243,15 +268,37 @@ async function gitUserDetails(giturl, id) {
   try {
     const response = await axios.get(giturl);
     const allUserData = response.data;
-    const { login, name, bio, location, followers, following, public_repos, avatar_url, html_url } = allUserData;
+    const {
+      login,
+      name,
+      bio,
+      location,
+      followers,
+      following,
+      public_repos,
+      avatar_url,
+      html_url,
+    } = allUserData;
 
-    bot.sendMessage(id, `*Username*: ${login}\n*Name*: ${name}\n*Bio*: ${bio == null ? "There is no bio" : bio}\n*Location*: ${location == null ? "There is no location found" : location}\n*Followers*: ${followers}\n*Following*: ${following}\n*Public Repos*: ${public_repos}`, {
-      parse_mode: "Markdown",
-    });
-    bot.sendMessage(id, `Visit your profile from here : 😊 \n*gitHub url* : ${html_url}`, {
-      parse_mode: "Markdown",
-    });
-    
+    bot.sendMessage(
+      id,
+      `*Username*: ${login}\n*Name*: ${name}\n*Bio*: ${
+        bio == null ? "There is no bio" : bio
+      }\n*Location*: ${
+        location == null ? "There is no location found" : location
+      }\n*Followers*: ${followers}\n*Following*: ${following}\n*Public Repos*: ${public_repos}`,
+      {
+        parse_mode: "Markdown",
+      }
+    );
+    bot.sendMessage(
+      id,
+      `Visit your profile from here : 😊 \n*gitHub url* : ${html_url}`,
+      {
+        parse_mode: "Markdown",
+      }
+    );
+
     bot.sendPhoto(id, avatar_url, {
       parse_mode: "Markdown",
     });
@@ -264,12 +311,15 @@ async function weatherReport(weatherUrl, id) {
   try {
     const response = await axios.get(weatherUrl);
     const allData = response.data;
-    const { weather , name, main, wind, sys} = allData;
+    const { weather, name, main, wind, sys } = allData;
     console.log(allData, "weather");
-    bot.sendMessage(id, `You want to know about *${name}, ${sys.country} * weather ☁️ , here is the mini weather report of *${name}* . \n*Type*: ${weather[0].main} \n*Description* : ${weather[0].description} \n*Temperature* : ${main.temp}°C\n*Feels like* : ${main.feels_like}°C\n*Min temperature* : ${main.temp_min}°C\n*Max temperature* : ${main.temp_max}°C\n*Max temperature* : ${main.humidity}%\n*Wind* : ${wind.speed}km/h`, {
-      parse_mode: "Markdown",
-    });
-  
+    bot.sendMessage(
+      id,
+      `You want to know about *${name}, ${sys.country} * weather ☁️ , here is the mini weather report of *${name}* . \n*Type*: ${weather[0].main} \n*Description* : ${weather[0].description} \n*Temperature* : ${main.temp}°C\n*Feels like* : ${main.feels_like}°C\n*Min temperature* : ${main.temp_min}°C\n*Max temperature* : ${main.temp_max}°C\n*Max temperature* : ${main.humidity}%\n*Wind* : ${wind.speed}km/h`,
+      {
+        parse_mode: "Markdown",
+      }
+    );
   } catch (error) {
     console.error("Error fetching GitHub user details:", error);
   }
@@ -281,7 +331,7 @@ bot.onText(/\/reminder (.+) at (.+)/, (message, match) => {
   const reminderTime = match[2];
 
   // Parse the reminder time (assuming format: HH:MM)
-  const [hours, minutes] = reminderTime.split(':').map(Number);
+  const [hours, minutes] = reminderTime.split(":").map(Number);
 
   // Set up the reminder schedule
   const reminderDate = new Date();
@@ -294,15 +344,14 @@ bot.onText(/\/reminder (.+) at (.+)/, (message, match) => {
 
     // Show notification
     notifier.notify({
-      title: 'Reminder',
+      title: "Reminder",
       message: reminderText,
       sound: true,
     });
   });
 
-  bot.sendMessage(chatId, 'Reminder set successfully!');
+  bot.sendMessage(chatId, "Reminder set successfully!");
 });
-
 
 // WITHOUT NOTIFICATION FEATURE
 // bot.onText(/\/reminder (.+) at (.+)/, (message, match) => {
@@ -334,8 +383,6 @@ bot.onText(/\/reminder (.+) at (.+)/, (message, match) => {
 //   client_email: process.env.GOOGLE_CLIENT_EMAIL,
 //   private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
 // };
-
-
 
 // Create a new Google Calendar client
 // const calendar = google.calendar({
